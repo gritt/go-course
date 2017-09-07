67b1897774c9a685190982dc338c18503188fc97
Gilvan <gilvanritter@gmail.com>
2017-09-07 15:29:50 -0300

    fill the deck of cards, learn loop logic, deal with arrays, unnused vars

---
+++ cards/deck.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+type deck []string
+
+func newDeck() deck {
+
+	cards := deck{}
+
+	cardSuits := []string{"Spades", "Hearts", "Diamonds", "Clubs"}
+	cardValues := []string{"Ace", "Two", "Three", "Four"}
+
+	// has to replace vars which are not used by _
+	for _, suit := range cardSuits {
+		for _, value := range cardValues {
+			cards = append(cards, value+" of "+suit)
+		}
+	}
+
+	return cards
+}
+
+func (d deck) print() {
+	for i, card := range d {
+		fmt.Println(i, card)
+	}
+}
--- cards/main.go
+++ cards/main.go
@@ -1,17 +1,8 @@
 package main
 
-import "fmt"
-
 func main() {
 
-	cards := []string{"Ace of Diamonds", newCard()}
-	cards = append(cards, "Six of Spades")
+	cards := newDeck()
 
-	for i, card := range cards {
-		fmt.Println(i, card)
-	}
-}
-
-func newCard() string {
-	return "Five of Diamonds"
+	cards.print()
 }
