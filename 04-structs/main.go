05becd69dc3798fe5c27881ecff6b040d5fef9f0
Gilvan <gilvanritter@gmail.com>
2017-09-10 23:32:44 -0300

    learning structs in go, created for person

--- 04-structs/main.go
+++ 04-structs/main.go
@@ -1,6 +1,24 @@
 package main
 
+import "fmt"
+
+type person struct {
+	firstName string
+	lastName string
+}
+
 func main() {
 
-	
+	// populate strings
+	//alex := person{"Alex", "Anderson"}
+	//alex := person{firstName: "Alex", lastName: "Anderson"}
+
+	// attributes are defined, but nil
+	var alex person
+	//alex.firstName = "Alex"
+	//alex.lastName = "Anderson"
+
+
+	// print struct and attributes
+	fmt.Printf("%+v", alex)
 }
