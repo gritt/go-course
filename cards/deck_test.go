f06a7b53be9e90d79e1b33d2d7b0369c1d0e0fed
Gilvan <gilvanritter@gmail.com>
2017-09-10 21:00:00 -0300

    added new test

--- cards/deck_test.go
+++ cards/deck_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"testing"
+	"os"
+)
 
 // t is the test handler
 func TestNewDeck(t *testing.T) {
@@ -19,4 +22,20 @@ func TestNewDeck(t *testing.T) {
 	if d[len(d)-1] != "Four of Clubs" {
 		t.Errorf(" Expected first card of Four of Clubs, but got %v", d[len(d)-1])
 	}
+}
+
+// we have to manually deal with testing files
+func TestSaveToFileAndNewDeckFromFile(t *testing.T)  {
+
+	os.Remove("_decktesting")
+
+	d := newDeck()
+	d.saveToFile("_decktesting")
+	ld := newDeckFromFile("_decktesting")
+
+	if len(ld) != 16 {
+		t.Errorf("Expected deck length of 16, but got %v", len(d))
+	}
+
+	os.Remove("_decktesting")
 }
