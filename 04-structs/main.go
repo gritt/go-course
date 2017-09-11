d764a2cfc4efa22cbb1dbb81d69a4e2dd3eea9df
Gilvan <gilvanritter@gmail.com>
2017-09-11 01:01:14 -0300

    pointers, hot to get the mem address of a value, how to get the value of a mem address, how to update the value by passing the mem address pointer

--- 04-structs/main.go
+++ 04-structs/main.go
@@ -11,18 +11,18 @@ type person struct {
 	firstName string
 	lastName  string
 
-// same result
-//	contact contactInfo
+	// same result
+	//	contact contactInfo
 	contactInfo
 }
 
 func main() {
 
 	jim := person{
-		firstName: "jim",
-		lastName:  "silva",
+		firstName: "Jim",
+		lastName:  "Haskel",
 
-	    //contact : contactInfo {}
+		//contact : contactInfo {}
 
 		contactInfo: contactInfo{
 			email:   "jim@gmail.com",
@@ -30,6 +30,30 @@ func main() {
 		},
 	}
 
-	// print struct and attributes
-	fmt.Printf("%+v", jim)
+	jim.updateName("Joe")
+
+	// another way to deal
+	// creates a pointer to the mem location of jim
+	jimPointer := &jim
+	jimPointer.updateLastName("Doe")
+
+
+	jim.print()
 }
+
+// pointer to change the value of the actual person instance (mem address value)
+// without pointer, go creates a copy of the struct p to another mem block
+func (p *person) updateName(newFirstName string) {
+	p.firstName = newFirstName
+}
+
+func (pointerToPerson *person) updateLastName(newLastName string)  {
+	(*pointerToPerson).lastName = newLastName
+}
+
+func (p person) print() {
+	fmt.Printf("%+v", p)
+}
+
+// turn address into value with *address
+// turn value into address with &value
