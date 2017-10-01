d00f992358b284bca644ad3e0538affb9149a4b1
Gilvan <gilvanritter@gmail.com>
2017-09-30 21:27:32 -0300

    added dummy / wrong code, to fix with interfaces

--- 06-interfaces/main.go
+++ 06-interfaces/main.go
@@ -1,16 +1,41 @@
 package main
 
+import "fmt"
+
 type englishBot struct {}
 type spanishBot struct {}
 
 func main() {
+	eb := englishBot{}
+	sp := spanishBot{}
 
+
+	//todo, use interfaces to make reuse of printGreeting
+	printGreeting(eb)
+	printGreeting(sp)
 }
 
-func (eb englishBot)getGreeting() string {
+// cannot overload functions, even they have different signature (args)
+func printGreeting(eb englishBot) {
+	fmt.Println(eb.getGreeting())
+}
+
+func printGreeting(sp englishBot)  {
+	fmt.Println(sp.getGreeting())
+}
+
+
+
+// omitting the value "eb" as var will not be used
+func (englishBot)getGreeting() string {
+	// let's suppose this code is very specific / custom implementation
+	// cant be reused
 	return "Hi There"
 }
 
-func (sp spanishBot)getGreeting() string {
+// omitting the value "eb" as var will not be used
+func (spanishBot)getGreeting() string {
+	// let's suppose this code is very specific / custom implementation
+	// cant be reused
 	return "Hola"
 }
