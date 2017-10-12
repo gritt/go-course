6205c2465be64ce612faf4c741e0aa79fc7d75b2
Gilvan <gilvanritter@gmail.com>
2017-10-12 01:48:45 -0300

    shorten code by making use of os.Stdout instead of implementing custom Writer custom to satisfy the Writer interface

--- 09-assignment-three/main.go
+++ 09-assignment-three/main.go
@@ -1,21 +1,15 @@
 package main
 
 import (
-	"fmt"
 	"io"
 	"log"
 	"os"
 )
 
-type fileWriter struct{}
-
 func main() {
 
 	// second index is the file path
 	fp := os.Args[1]
-
-	// implements the Writer interface, so it can write the output
-	lw := fileWriter{}
 
 	// first param implmenets Writer, second param implement Reader
 	f, err := os.Open(fp)
@@ -24,11 +18,5 @@ func main() {
 	}
 
 	// reads input, copy (write) to output
-	io.Copy(lw, f)
-}
-
-// fileWriter is now implemeting the Writer interface
-func (fileWriter) Write(bs []byte) (int, error) {
-	fmt.Println(string(bs))
-	return len(bs), nil
+	io.Copy(os.Stdout, f)
 }
