f8a37e0241cc39f3f8db28a181b21019e9f3eefa
Gilvan <gilvanritter@gmail.com>
2019-04-12 17:40:36 -0300

    added comments and example of function types that can implements interfaces, they can wrap/delegate/adapt other kinds of logic and make it compatible with the required interface, NICE!

--- 06-interfaces/main.go
+++ 06-interfaces/main.go
@@ -7,17 +7,32 @@ import "fmt"
 type bot interface {
 	getGreeting() string
 }
-type englishBot struct {}
-type spanishBot struct {}
+type englishBot struct{}
+type spanishBot struct{}
+
+// [1] make any function that returns a string compatible with botGenerator
+type botGenerator func() string
+
+// [2] AND, receives the `outdated` function and run in wrapped in getGreeting, making it now an implementation of bot interface
+func (p botGenerator) getGreeting() string {
+	return p()
+}
+
+// THIS -> is the outdated function that needs to be "wrapped" to be compatible with the needed interface (bot interface)
+func getBomDia() string {
+	return "Bom dia"
+}
 
 func main() {
 	eb := englishBot{}
 	sp := spanishBot{}
 
+	printGreeting(botGenerator(getBomDia))
 
 	// both bots use bot interface
 	printGreeting(eb)
 	printGreeting(sp)
+
 }
 
 // receive the interface as type
@@ -26,14 +41,14 @@ func printGreeting(b bot) {
 }
 
 // omitting the value "eb" as var will not be used
-func (englishBot)getGreeting() string {
+func (englishBot) getGreeting() string {
 	// let's suppose this code is very specific / custom implementation
 	// cant be reused
 	return "Hi There"
 }
 
 // omitting the value "eb" as var will not be used
-func (spanishBot)getGreeting() string {
+func (spanishBot) getGreeting() string {
 	// let's suppose this code is very specific / custom implementation
 	// cant be reused
 	return "Hola"
